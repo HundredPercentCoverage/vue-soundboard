152bc17976e39b44af4dd040fd5dbe52b6d4c8f0
HundredPercentCoverage <bunglenutter@gmail.com>
2023-03-20 23:38:53 +0000

    Pause playback

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const props = defineProps({ src: String })
 
-const audio = ref(new Audio(props.src));
+const audio = new Audio(props.src)
+const isPlaying = ref(false)
+
+function playAudio() {
+  if (isPlaying.value) {
+    audio.pause()
+    audio.currentTime = 0
+    isPlaying.value = false
+  } else {
+    audio.play()
+    isPlaying.value = true
+  }
+}
 </script>
 
 <template>
-  <button @click="() => audio.play()">Play</button>
+  <button @click="playAudio">{{ isPlaying ? 'Pause' : 'Play' }}</button>
 </template>
